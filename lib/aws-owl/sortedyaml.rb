8e7cb2bb2d0defe162e5726af8eb76ecfa393f73
Andy Brody <andy@stripe.com>
2012-01-17 17:53:52 -0800

    Sort yaml hashes by key.

---
+++ lib/aws-owl/sortedyaml.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+
+class Hash
+  # Replace the to_yaml function so it sorts hashes by keys
+  #
+  # Original function is in /usr/lib/ruby/1.8/yaml/rubytypes.rb
+  def to_yaml( opts = {} )
+    YAML::quick_emit( object_id, opts ) do |out|
+      out.map( taguri, to_yaml_style ) do |map|
+        sort.each do |k, v|   # <-- here's my addition (the 'sort')
+          map.add( k, v )
+        end
+      end
+    end
+  end
+end
